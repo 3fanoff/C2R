d9bf706809b51a3b91033bd6da2b80208552b577
trifanov <trifanov@teamidea.ru>
2024-10-29 16:02:08 +0300

    layout of main blocks p.2 * contacts * articles carousel * socials box

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,15 @@
 import '@scss/main.app.scss';
+import CarouselGallery from "./carouselGallery";
+
+let carousels = null;
+document.addEventListener('DOMContentLoaded', () => {
+    if ((carousels = document.querySelectorAll('.js-carousel-gallery')).length) {
+        Array.from(carousels).forEach((carousel) => {
+           new CarouselGallery(carousel).init();
+        });
+    }
+})
+
 
 /*
 document.body.style.setProperty('--view-width', document.documentElement.clientWidth);
